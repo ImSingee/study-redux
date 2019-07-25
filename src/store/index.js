31978f61feef0e752913badec487eaf43e605d8a
Singee <imsingee@gmail.com>
2019-07-25 20:27:10 +0800

    16 安装 redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducer';
+import thunk from 'redux-thunk';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+const enhancer = composeEnhancer(applyMiddleware(thunk));
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    enhancer
 );
 
 export default store;
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,3 @@
-import uuid from 'uuid/v1';
-
 const defaultState = {
     list: []
 };
