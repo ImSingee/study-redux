1dc7b4f98312fff0d939034ba75649ed086eb1b5
Singee <imsingee@gmail.com>
2019-07-25 19:51:09 +0800

    14 UI 设置为无状态组件

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,38 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Input, Button, List } from 'antd';
 import 'antd/dist/antd.css';
 
-class TodoListUI extends Component {
-    render() {
-        return (
-            <div style={{ margin: '30px' }}>
-                <h1>TodoList</h1>
-                <div>
-                    <Input
-                        style={{ width: '200px', marginRight: '10px' }}
-                        onInput={this.props.changeInputValue}
-                        value={this.props.inputValue}
-                    />
-                    <Button
-                        type="primary"
-                        onClick={this.props.add}
-                        disabled={!this.props.inputValue}
-                    >增加</Button>
-                </div>
-                <div>
-                    <List
-                        style={{ width: '300px', marginTop: '10px' }}
-                        bordered
-                        dataSource={this.props.list}
-                        renderItem={item => <List.Item
-                            data-id={item.id}
-                            onClick={this.props.clickItem}
-                        >{item.name}</List.Item>}
-                    />
-                </div>
-            </div>
-        );
-    }
-}
+const TodoListUI = (props) => (
+    <div style={{ margin: '30px' }}>
+        <h1>TodoList</h1>
+        <div>
+            <Input
+                style={{ width: '200px', marginRight: '10px' }}
+                onInput={props.changeInputValue}
+                value={props.inputValue}
+            />
+            <Button
+                type="primary"
+                onClick={props.add}
+                disabled={!props.inputValue}
+            >增加</Button>
+        </div>
+        <div>
+            <List
+                style={{ width: '300px', marginTop: '10px' }}
+                bordered
+                dataSource={props.list}
+                renderItem={item => <List.Item
+                    data-id={item.id}
+                    onClick={props.clickItem}
+                >{item.name}</List.Item>}
+            />
+        </div>
+    </div>
+);
 
 export default TodoListUI;
