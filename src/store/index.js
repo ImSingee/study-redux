56ec8abcb106993db545c2c8c5f648fe57205225
Singee <imsingee@gmail.com>
2019-07-25 20:49:41 +0800

    18 安装 redux-saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import reducer from './reducer';
-import thunk from 'redux-thunk';
+import sagas from './sagas';
 
+const sagaMiddleware = createSagaMiddleware();
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
-const enhancer = composeEnhancer(applyMiddleware(thunk));
+const enhancer = composeEnhancer(applyMiddleware(sagaMiddleware));
 
 const store = createStore(
     reducer,
     enhancer
 );
+
+sagaMiddleware.run(sagas);
 
 export default store;
---
+++ src/store/sagas.js
@@ -0,0 +1,3 @@
+function* MySaga() { };
+
+export default MySaga;
